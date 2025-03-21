5eff063411e585cd41fbb97ed9b1a7e4ef0f692f
angel <engelpromise@gmail.com>
2025-03-21 15:55:41 -0500

    proyecto actualizado

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 # Inserta tu dirección de URL sin la barra diagonal al final
-URL_SERVICE = "https://cnt-0045ba15-491c-41f4-a484-19780e37f834.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-2e244039-d377-4402-8741-c3609d33b3d9.containerhub.tripleten-services.com"
 # Almacena la ruta para crear un usuario o usuaria en esta variable
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
